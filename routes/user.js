20a383a40a9a52f609d5c9401f71dcefa100ca32
nayan <nayanakbari2003@gmail.com>
2024-04-01 22:58:03 +0530

    third

--- routes/user.js
+++ routes/user.js
@@ -12,5 +12,7 @@ router.post("/new",createNewUser);
 router.get("/:id",findUserById);
 router.put("/:id",updateUserById);
 router.delete("/:id",deleteUserById);
+// router.route("/user/:id").get(findUserById).put(updateUserById).delete(deleteUserById)
+//same as above three lines
 
 export default router;
